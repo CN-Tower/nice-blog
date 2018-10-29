764f59881020bc48e31e68e11d85653105341bb8
CN-Tower <cntower@yahoo.com>
2018-10-29 15:10:13 +0800

    Update category.component.ts

--- client/src/app/sidebar/category/category.component.ts
+++ client/src/app/sidebar/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NzFormatEmitEvent, NzTreeNode } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-category',
@@ -6,8 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.less']
 })
 export class CategoryComponent implements OnInit {
+  nodes: any[] = [ {
+    title   : 'parent 1',
+    key     : '100',
+    expanded: true,
+    children: [ {
+      title   : 'parent 1-0',
+      key     : '1001',
+      expanded: true,
+      children: [
+        { title: 'leaf', key: '10010', isLeaf: true },
+        { title: 'leaf', key: '10011', isLeaf: true }
+      ]
+    }, {
+      title   : 'parent 1-1',
+      key     : '1002',
+      children: [
+        { title: 'leaf', key: '10020', isLeaf: true }
+      ]
+    }]
+  }, {
+    title   : 'parent 2',
+    key     : '100',
+    expanded: true,
+    children: [ {
+      title   : 'parent 2-0',
+      key     : '1001',
+      expanded: true,
+      children: [
+        { title: 'leaf', key: '20010', isLeaf: true },
+        { title: 'leaf', key: '20011', isLeaf: true }
+      ]
+    }, {
+      title   : 'parent 2-1',
+      key     : '2002',
+      children: [
+        { title: 'leaf', key: '20020', isLeaf: true }
+      ]
+    } ]
+  }];
 
   constructor() { }
+
+  nzEvent(event: NzFormatEmitEvent): void {
+    console.log(event);
+  }
 
   ngOnInit() {
   }
