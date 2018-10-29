a46314ec52401fb196274e913a569578919e05ff
CN-Tower <cntower@yahoo.com>
2018-10-29 15:07:00 +0800

    Update home.component.ts

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,25 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [`
-    [nz-carousel-content] {
-      text-align: center;
-      height: 270px;
-      line-height: 160px;
-      background: #364d79;
-      color: #fff;
-      overflow: hidden;
-    }
-    h3 {
-      color: #fff;
-    }
-  `]
+  styleUrls: ['./home.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
   array = [ 1, 2, 3, 4 ];
-
+  data = new Array(7).fill({}).map((i, index) => {
+    return {
+      href: 'http://ant.design',
+      title: `ant design part ${index}`,
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      description: 'Ant Design, a design language for background applications.',
+      content: 'We supply a series of design principles, practical patterns and high quality design resources.'
+    };
+  });
   constructor() { }
 
   ngOnInit() {
