51e8a503a15956adb508f49d86c273dfd92a149e
CN-Tower <cntower@yahoo.com>
2018-11-01 09:32:12 +0800

    Update sidebar.module.ts

--- client/src/app/sidebar/sidebar.module.ts
+++ client/src/app/sidebar/sidebar.module.ts
@@ -5,14 +5,17 @@ import { UserComponent } from './user/user.component';
 import { HotComponent } from './hot/hot.component';
 import { ArchiveComponent } from './archive/archive.component';
 import { TagsComponent } from './tags/tags.component';
-import { RecentComponent } from './recent/recent.component';
+import { ActiveComponent } from './active/active.component';
 import { SidebarComponent } from './sidebar.component';
 import { LoginComponent } from './login/login.component';
 import { CategoryComponent } from './category/category.component';
+import { AvatarComponent } from './avatar/avatar.component';
+import { MetaComponent } from './meta/meta.component';
 
 const SIDEBAR_COMPONENTS = [
   UserComponent, HotComponent, ArchiveComponent, TagsComponent,
-  RecentComponent, SidebarComponent, LoginComponent, CategoryComponent
+  ActiveComponent, SidebarComponent, LoginComponent, CategoryComponent,
+  AvatarComponent, MetaComponent
 ];
 
 @NgModule({
