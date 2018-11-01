06c4ddaaea296c69b2b2d2ee58dd42158617915d
CN-Tower <cntower@yahoo.com>
2018-11-01 09:35:10 +0800

    Create meta.component.ts

---
+++ client/src/app/sidebar/meta/meta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meta',
+  templateUrl: './meta.component.html',
+  styleUrls: ['./meta.component.less']
+})
+export class MetaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
