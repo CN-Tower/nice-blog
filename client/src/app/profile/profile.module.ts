8e6753b0e5c6f8090eb9227ae3cbfb8ef40efee9
CN-Tower <cntower@yahoo.com>
2018-11-01 09:19:25 +0800

    Update profile.module.ts

--- client/src/app/profile/profile.module.ts
+++ client/src/app/profile/profile.module.ts
@@ -3,10 +3,28 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { ProfileComponent } from './profile.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { ArticleComponent } from './article/article.component';
+import { CollectionComponent } from './collection/collection.component';
+import { FollowersComponent } from './followers/followers.component';
+import { FollowingComponent } from './following/following.component';
+import { SettingComponent } from './setting/setting.component';
 
 const routers: Routes = [
-  { path: '',   component: ProfileComponent },
-  { path: '**', component: ProfileComponent }
+  { path: '', component: ProfileComponent,
+    children: [
+      { path: 'user/:userId', component: ArticleComponent },
+      { path: 'user/:userId/article', component: ArticleComponent },
+      { path: 'user/:userId/collection', component: CollectionComponent },
+      { path: 'user/:userId/followers', component: FollowersComponent },
+      { path: 'user/:userId/following', component: FollowingComponent },
+      { path: 'history', component: CalendarComponent },
+      { path: 'followers', component: FollowersComponent },
+      { path: 'following', component: FollowingComponent },
+      { path: 'setting', component: SettingComponent },
+      { path: '', component: CalendarComponent }
+    ]
+  }
 ];
 
 @NgModule({
@@ -15,6 +33,9 @@ const routers: Routes = [
     SharedModule,
     RouterModule.forChild(routers)
   ],
-  declarations: [ProfileComponent]
+  declarations: [
+    ProfileComponent, CalendarComponent, ArticleComponent,
+    CollectionComponent, FollowersComponent, FollowingComponent, SettingComponent
+  ]
 })
 export class ProfileModule { }
