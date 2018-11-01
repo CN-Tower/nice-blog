de3c555f308d6fe50e7c23827cf7cf9272744614
CN-Tower <cntower@yahoo.com>
2018-11-01 15:57:32 +0800

    Update my-collection.component.ts

--- client/src/app/article/my-collection/my-collection.component.ts
+++ client/src/app/article/my-collection/my-collection.component.ts
@@ -7,6 +7,14 @@ import { ArticleService } from '../article.service';
   styleUrls: ['./my-collection.component.less']
 })
 export class MyCollectionComponent implements OnInit {
+  allChecked = false;
+  disabledButton = true;
+  checkedNumber = 0;
+  displayData: Array<{ name: string; age: number; address: string; checked: boolean }> = [];
+  operating = false;
+  dataSet = [];
+  indeterminate = false;
+
   data = new Array(7).fill({}).map((i, index) => {
     return {
       href: 'http://ant.design',
@@ -19,6 +27,42 @@ export class MyCollectionComponent implements OnInit {
 
   constructor(public articleService: ArticleService) { }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+    for (let i = 0; i < 46; i++) {
+      this.dataSet.push({
+        name   : `Edward King ${i}`,
+        age    : 32,
+        address: `London, Park Lane no. ${i}`,
+        checked: false
+      });
+    }
+  }
+
+  currentPageDataChange($event: Array<{ name: string; age: number; address: string; checked: boolean }>): void {
+    this.displayData = $event;
+  }
+
+  refreshStatus(): void {
+    const allChecked = this.displayData.every(value => value.checked === true);
+    const allUnChecked = this.displayData.every(value => !value.checked);
+    this.allChecked = allChecked;
+    this.indeterminate = (!allChecked) && (!allUnChecked);
+    this.disabledButton = !this.dataSet.some(value => value.checked);
+    this.checkedNumber = this.dataSet.filter(value => value.checked).length;
+  }
+
+  checkAll(value: boolean): void {
+    this.displayData.forEach(data => data.checked = value);
+    this.refreshStatus();
+  }
+
+  operateData(): void {
+    this.operating = true;
+    setTimeout(_ => {
+      this.dataSet.forEach(value => value.checked = false);
+      this.refreshStatus();
+      this.operating = false;
+    }, 1000);
+  }
 
 }
