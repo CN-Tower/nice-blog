b1d327c042f181fb5dad6197c41e3ef32070cbfc
CN-Tower <cntower@yahoo.com>
2018-11-01 09:09:44 +0800

    Update explore.component.ts

--- client/src/app/explore/explore.component.ts
+++ client/src/app/explore/explore.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ExploreService } from './explore.service';
 
 @Component({
   selector: 'app-explore',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./explore.component.less']
 })
 export class ExploreComponent implements OnInit {
+  isShowFilter: Boolean = this.exploreService.toggleFilter.isShow;
+  viewRouter: 'find-article' | 'find-user';
 
-  constructor() { }
+  constructor(private router: Router, private exploreService: ExploreService) { }
 
   ngOnInit() {
+    if (this.router.url.includes('/find-user')) {
+      this.viewRouter = 'find-user';
+    } else {
+      this.viewRouter = 'find-article';
+    }
   }
 
+  toggleFilter() {
+    this.isShowFilter = !this.isShowFilter;
+    this.exploreService.toggleFilter.isShow = this.isShowFilter;
+  }
+
+  onViewChange() {
+    this.router.navigateByUrl(`/explore/${this.viewRouter}`);
+  }
 }
