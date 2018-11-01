7f59905a34e1331be13643cd529f5578243336e8
CN-Tower <cntower@yahoo.com>
2018-11-01 09:21:45 +0800

    Create calendar.component.ts

---
+++ client/src/app/profile/calendar/calendar.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar',
+  templateUrl: './calendar.component.html',
+  styleUrls: ['./calendar.component.less']
+})
+export class CalendarComponent implements OnInit {
+  listDataMap = {
+    eight : [
+      { type: 'warning', content: 'This is warning event.' },
+      { type: 'success', content: 'This is usual event.' }
+    ],
+    ten   : [
+      { type: 'warning', content: 'This is warning event.' },
+      { type: 'success', content: 'This is usual event.' },
+      { type: 'error', content: 'This is error event.' }
+    ],
+    eleven: [
+      { type: 'warning', content: 'This is warning event' },
+      { type: 'success', content: 'This is very long usual event........' },
+      { type: 'error', content: 'This is error event 1.' },
+      { type: 'error', content: 'This is error event 2.' },
+      { type: 'error', content: 'This is error event 3.' },
+      { type: 'error', content: 'This is error event 4.' }
+    ]
+  };
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getMonthData(date: Date): number | null {
+    if (date.getMonth() === 8) {
+      return 1394;
+    }
+    return null;
+  }
+}
