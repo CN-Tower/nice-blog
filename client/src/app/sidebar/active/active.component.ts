5bd5a34ce2b3260acefd0e57f6f7b7cf81b917a2
CN-Tower <cntower@yahoo.com>
2018-11-01 09:33:32 +0800

    Create active.component.ts

---
+++ client/src/app/sidebar/active/active.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-active',
+  templateUrl: './active.component.html',
+  styleUrls: ['./active.component.less']
+})
+export class ActiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
