ff12bf59f0ccc68c7b75e14ab72954077b07ff46
CN-Tower <cntower@yahoo.com>
2018-11-01 08:43:46 +0800

    Update article.component.ts

--- client/src/app/article/article.component.ts
+++ client/src/app/article/article.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ArticleService } from './article.service';
 
 @Component({
   selector: 'app-article',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.less']
 })
 export class ArticleComponent implements OnInit {
+  isShowFilter: Boolean = this.articleService.toggleFilter.isShow;
+  viewRouter: 'my-article' | 'my-collection';
 
-  constructor() { }
+  constructor( private router: Router, private articleService: ArticleService) { }
 
   ngOnInit() {
+    if (this.router.url.includes('/my-collection')) {
+      this.viewRouter = 'my-collection';
+    } else {
+      this.viewRouter = 'my-article';
+    }
   }
 
+  toggleFilter() {
+    this.isShowFilter = !this.isShowFilter;
+    this.articleService.toggleFilter.isShow = this.isShowFilter;
+  }
+
+  onChangeView() {
+    this.router.navigateByUrl(`/article/${this.viewRouter}`);
+  }
 }
