c123130078355239af0eac5903adfc5876b753d3
CN-Tower <cntower@yahoo.com>
2018-11-05 17:09:12 +0800

    Create register.component.ts

---
+++ client/src/app/sidebar/login/register/register.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NzDrawerRef } from 'ng-zorro-antd';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.less']
+})
+export class RegisterComponent implements OnInit {
+  validateForm: FormGroup;
+  value: any;
+
+  constructor(private fb: FormBuilder, private drawerRef: NzDrawerRef<any>) { }
+
+  ngOnInit() {
+    this.validateForm = this.fb.group({
+      email            : [ null, [ Validators.email ] ],
+      password         : [ null, [ Validators.required ] ],
+      checkPassword    : [ null, [ Validators.required, this.confirmationValidator ] ],
+      nickname         : [ null, [ Validators.required ] ],
+      phoneNumberPrefix: [ '+86' ],
+      websetPrefix     : [ 'http://' ],
+      phoneNumber      : [ null, [ Validators.required ] ],
+      website          : [ null, [ Validators.required ] ],
+      captcha          : [ null, [ Validators.required ] ],
+      agree            : [ false ]
+    });
+  }
+
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(i)) {
+        this.validateForm.controls[ i ].markAsDirty();
+        this.validateForm.controls[ i ].updateValueAndValidity();
+      }
+    }
+  }
+
+  updateConfirmValidator(): void {
+    /** wait for refresh value */
+    Promise.resolve().then(() => this.validateForm.controls.checkPassword.updateValueAndValidity());
+  }
+
+  confirmationValidator = (control: FormControl): { [ s: string ]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.validateForm.controls.password.value) {
+      return { confirm: true, error: true };
+    }
+  }
+
+  getCaptcha(e: MouseEvent): void {
+    e.preventDefault();
+  }
+
+  close(): void {
+    this.drawerRef.close(this.value);
+  }
+}
