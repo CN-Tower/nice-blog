abcb37f80c6341b131762253523663e93736a0c7
CN-Tower <cntower@yahoo.com>
2018-11-05 16:54:32 +0800

    Update login.component.ts

--- client/src/app/sidebar/login/login.component.ts
+++ client/src/app/sidebar/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzDrawerService } from 'ng-zorro-antd';
+import { RegisterComponent } from './register/register.component';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
+  validateForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private drawerService: NzDrawerService) { }
 
   ngOnInit() {
+    this.validateForm = this.fb.group({
+      userName: [ null, [ Validators.required ] ],
+      password: [ null, [ Validators.required ] ],
+      captcha: [ null, [ Validators.required ] ],
+      remember: [ true ]
+    });
   }
 
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      if (this.validateForm.controls.hasOwnProperty(i)) {
+        this.validateForm.controls[ i ].markAsDirty();
+        this.validateForm.controls[ i ].updateValueAndValidity();
+      }
+    }
+  }
+
+  goToRegister() {
+    const drawerRef = this.drawerService.create<RegisterComponent, { value: string }, string>({
+      nzTitle: '用户注册',
+      nzContent: RegisterComponent,
+      nzWidth: '520px'
+    });
+
+    drawerRef.afterOpen.subscribe(() => {
+      console.log('Drawer(Component) open');
+    });
+
+    drawerRef.afterClose.subscribe(data => {
+      console.log(data);
+    });
+  }
 }
