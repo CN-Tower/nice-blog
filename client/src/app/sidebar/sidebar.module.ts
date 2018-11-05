fcf3f70550619a7e7604d2e487c45def656534cc
CN-Tower <cntower@yahoo.com>
2018-11-05 16:53:35 +0800

    Update sidebar.module.ts

--- client/src/app/sidebar/sidebar.module.ts
+++ client/src/app/sidebar/sidebar.module.ts
@@ -12,12 +12,12 @@ import { CategoryComponent } from './category/category.component';
 import { AvatarComponent } from './avatar/avatar.component';
 import { MetaComponent } from './meta/meta.component';
 import { RecentComponent } from './recent/recent.component';
-
+import { RegisterComponent } from './login/register/register.component';
 
 const SIDEBAR_COMPONENTS = [
   UserComponent, HotComponent, ArchiveComponent, TagsComponent,
   ActiveComponent, SidebarComponent, LoginComponent, CategoryComponent,
-  AvatarComponent, MetaComponent, RecentComponent
+  AvatarComponent, MetaComponent, RecentComponent, RegisterComponent
 ];
 
 @NgModule({
@@ -26,6 +26,7 @@ const SIDEBAR_COMPONENTS = [
     SharedModule
   ],
   declarations: SIDEBAR_COMPONENTS,
+  entryComponents: [ RegisterComponent ],
   exports: SIDEBAR_COMPONENTS
 })
 export class SidebarModule { }
