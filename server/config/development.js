88088c364edea383ded4550afe54e77f0cb4c2cf
CN-Tower <cntower@yahoo.com>
2018-11-14 16:27:32 +0800

    Update development.js

--- server/config/development.js
+++ server/config/development.js
@@ -1,7 +1,7 @@
 module.exports = {
 	port: 3000,
-  dbUrl: 'mongodb://10.40.154.118:26001/nice-blog',
-  secret: 'secret',
+	dbUrl: 'mongodb://10.40.154.118:26001/nice-blog',
+	secret: 'secret',
 	session: {
 		name: 'SID',
 		secret: 'SID',
