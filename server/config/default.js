6c26303e1e4d5002cdaa9c54c85e7325af42787f
CN-Tower <cntower@yahoo.com>
2018-11-14 16:23:24 +0800

    Update default.js

--- server/config/default.js
+++ server/config/default.js
@@ -1,13 +1,10 @@
 module.exports = {
 	port: 3000,
-	dbUrl: 'mongodb://localhost:27017/elm',
+  dbUrl: '',
+  secret: '',
 	session: {
-		name: 'SID',
-		secret: 'SID',
-		cookie: {
-			httpOnly: true,
-		    secure: false,
-		    maxAge: 365 * 24 * 60 * 60 * 1000,
-		}
+		name: '',
+		secret: '',
+		cookie: { maxAge: 60 * 60 * 6000 }
 	}
 }
