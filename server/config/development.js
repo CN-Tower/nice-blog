f8ad107a27673806a11a8d89d35605529b567706
CN-Tower <cntower@yahoo.com>
2018-11-14 16:23:09 +0800

    Create development.js

---
+++ server/config/development.js
@@ -0,0 +1,10 @@
+module.exports = {
+	port: 3000,
+  dbUrl: 'mongodb://10.40.154.118:26001/nice-blog',
+  secret: 'secret',
+	session: {
+		name: 'SID',
+		secret: 'SID',
+		cookie: { maxAge: 60 * 60 * 6000 }
+	}
+}
