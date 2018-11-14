513b409ce3b7ec92197c6d0a28810a919c52eb90
CN-Tower <cntower@yahoo.com>
2018-11-14 16:49:03 +0800

    Update article.js

--- server/models/article.js
+++ server/models/article.js
@@ -5,18 +5,18 @@ const Schema = mongoose.Schema;
 class ArticleSchema extends Schema {
   constructor() {
     super({
-      title:        { type: String, required: [true, "can't be blank"] },
-      tabs:         { type: Array, default: [] },
-      description:  { type: String, default: '' },
-      body:         { type: String, required: [true, "can't be blank"] },
-      owner:        { type: Schema.Types.ObjectId, ref: 'User', required: [true, "Can't be blank"], index: true },
-      author:       { type: Schema.Types.ObjectId, ref: 'User' },
-      stars:        [{ type: Schema.Types.ObjectId, ref: 'User' }],
-      favorites:    [{ type: Schema.Types.ObjectId, ref: 'User' }],
-      images:       { type: Array, default: [] },
-      isRetweet:    { type: Boolean, default: false },
+      title:     { type: String, required: [true, "can't be blank"] },
+      tabs:      { type: Array, default: [] },
+      description: { type: String, default: '' },
+      body:      { type: String, required: [true, "can't be blank"] },
+      owner:     { type: Schema.Types.ObjectId, ref: 'User', required: [true, "Can't be blank"], index: true },
+      author:    { type: Schema.Types.ObjectId, ref: 'User' },
+      stars:     [{ type: Schema.Types.ObjectId, ref: 'User' }],
+      favorites: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+      images:    { type: Array, default: [] },
+      isRetweet: { type: Boolean, default: false },
       collectedUserIds: { type: Array, default: [] },
-      commentId:    { type: String, default: '' }
+      commentId: { type: String, default: '' }
     }, { timestamps: true });
 
     this.methods = fn.extend({}, this, []);
