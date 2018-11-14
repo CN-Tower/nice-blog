d09b31e2f9c6294981f05165d73b2f79860bd44e
CN-Tower <cntower@yahoo.com>
2018-11-14 16:29:00 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,41 +1,58 @@
-import createError from 'http-errors';
 import express from 'express';
+import fn from 'funclib';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import session from 'express-session';
+import router from './routes/index';
+import config from 'config-lite';
+import './mongodb';
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.all('*', (req, res, next) => {
+	res.header("X-Powered-By", '1.0.1')
+	if (req.method == 'OPTIONS') {
+    res.status(200).send({ message: 'ok' });
+	} else {
+    next();
+	}
+});
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  name: config.session.name,
+  secret: config.session.secret,
+  cookie: config.session.cookie,
+  resave: false,
+  saveUninitialized: false
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api/v1.0', router);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+if (process.env.NODE_ENV === 'production') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({'errors': { message: err.message, error: {} }});
+  });
+} else {
+  app.use(function(err, req, res, next) {
+    // fn.log(err.stack, 'Error Stack');
+    res.status(err.status || 500);
+    res.json({'errors': { message: err.message, error: err }});
+  });
+}
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(config.port, () => {
+  fn.log(`Listenig on port: ${config.port}`, { title: 'Nice-Blog-Server', color: 'green' });
 });
-
-module.exports = app;
