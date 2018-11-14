e383929ba310e4cce4ae0994855b535a6bf165c5
CN-Tower <cntower@yahoo.com>
2018-11-14 16:27:06 +0800

    Update production.js

--- server/config/production.js
+++ server/config/production.js
@@ -1,13 +1,10 @@
 module.exports = {
-	port: 8001,
-	dbUrl: 'mongodb://localhost:27017/elm',
+	port: 3000,
+	dbUrl: 'mongodb://localhost:27017/nice-blog',
+	secret: 'secret',
 	session: {
 		name: 'SID',
 		secret: 'SID',
-		cookie: {
-			httpOnly: true,
-		    secure: false,
-		    maxAge: 365 * 24 * 60 * 60 * 1000,
-		}
+		cookie: { maxAge: 6000 }
 	}
 }
