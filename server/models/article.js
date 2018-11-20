dba12609ba418a1e4d4ac66893dcf1d99cadcdfb
CN-Tower <cntower@yahoo.com>
2018-11-20 08:41:53 +0800

    Update article.js

--- server/models/article.js
+++ server/models/article.js
@@ -2,33 +2,57 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-class ArticleSchema extends Schema {
-  constructor() {
-    super({
-      title:     { type: String, required: [true, "can't be blank"] },
-      tabs:      { type: Array, default: [] },
-      description: { type: String, default: '' },
-      body:      { type: String, required: [true, "can't be blank"] },
-      owner:     { type: Schema.Types.ObjectId, ref: 'User', required: [true, "Can't be blank"], index: true },
-      author:    { type: Schema.Types.ObjectId, ref: 'User' },
-      stars:     [{ type: Schema.Types.ObjectId, ref: 'User' }],
-      favorites: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-      images:    { type: Array, default: [] },
-      isRetweet: { type: Boolean, default: false },
-      collectedUserIds: { type: Array, default: [] },
-      commentId: { type: String, default: '' }
-    }, { timestamps: true });
+const ArticleSchema = new Schema({
+  title:     { type: String, required: [true, "can't be blank"] },
+  tabs:      { type: Array, default: [] },
+  description: { type: String, default: '' },
+  body:      { type: String, required: [true, "can't be blank"] },
+  owner:     { type: Schema.Types.ObjectId, ref: 'User', required: [true, "Can't be blank"], index: true },
+  author:    { type: Schema.Types.ObjectId, ref: 'User' },
+  stars:     [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  favorites: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  images:    { type: Array, default: [] },
+  isRetweet: { type: Boolean, default: false },
+  collectedUserIds: { type: Array, default: [] },
+  commentId: { type: String, default: '' }
+}, {
+  timestamps: true,
+  collection: 'articles'
+});
 
-    this.methods = fn.extend({}, this, []);
-    this.statics = fn.extend({}, this, []);
+ArticleSchema.pre('validate', function(next){
+  if(!this.slug) this.slugify();
+  next();
+});
+
+class ArticleClass {
+  slugify() {
+    this.slug = slug(this.title) + '-' + (Math.random() * Math.pow(36, 6) | 0).toString(36);
   }
-  
-  
+
+  async updateFavoriteCount() {
+    const count = await User.count({favorites: {$in: [article._id]}});
+    this.favoritesCount = count;
+    return await this.save();
+  };
+
+  toJSONFor(user) {
+    return {
+      slug: this.slug,
+      title: this.title,
+      description: this.description,
+      body: this.body,
+      createdAt: this.createdAt,
+      updatedAt: this.updatedAt,
+      tagList: this.tagList,
+      favorited: user ? user.isFavorite(this._id) : false,
+      favoritesCount: this.favoritesCount,
+      author: this.author.toProfileJSONFor(user)
+    };
+  }
 }
 
+ArticleSchema.loadClass(ArticleClass);
 
-
-const articleSchema = new ArticleSchema();
-
-export default mongoose.model('Article', articleSchema);
+export default mongoose.model('Article', ArticleSchema);
 
