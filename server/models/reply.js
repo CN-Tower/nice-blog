298f68d0f4f71ec860dcfe2b5ca1b83288e48394
CN-Tower <cntower@yahoo.com>
2018-11-20 09:30:56 +0800

    Create reply.js

---
+++ server/models/reply.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const ReplySchema = new Schema({
+  author:     { type: Schema.Types.ObjectId, ref: 'User' },
+  replyTo:    { type: Schema.Types.ObjectId, ref: 'User' },
+  body:       { type: String, required: [true, "Can't be blank"], trim: true },
+  thumbUsers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+}, {
+  timestamps: true,
+  collection: 'replies'
+});
+
+class ReplyClass {
+  toJSONFor() {
+    return {
+      id: this._id,
+      body: this.body,
+      createdAt: this.createdAt,
+      author: this.author.toProfileJSON(),
+      replyTo: this.replyTo.toProfileJSON()
+    };
+  }
+}
+
+ReplySchema.loadClass(ReplyClass);
+
+export default mongoose.model('Comment', ReplySchema);
