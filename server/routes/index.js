db4818ec2e53c6428552c5cf1a7644ebfbd4c15f
CN-Tower <cntower@yahoo.com>
2018-11-20 09:38:28 +0800

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,29 @@
 import express from 'express';
-import User from '../controller/user';
-import Article from '../controller/article';
-import Comment from '../controller/comment';
+import validate from '../middlewares/validate';
+import auth from '../controller/auth'
+import user from '../controller/user'
+// import article from '../controller/article'
+// import comment from '../controller/comment'
 
 const router = express.Router();
 
-router.post('/test', User.register);
-// router.post('/test', (req, res, next) => {
-//   res.send(200, {'msg': 'ok'});
-// });
+router.use(validate);
+
+// 用户相关接口
+router.post('/user', user.register);
+router.post('/user/login', user.login);
+router.put('/user', auth.required, user.updateUser);
+router.get('/user', auth.required, user.getUserAuthInfo);
+// router.get('/users', auth.required, user.getUserAuthInfo);
+
+// router.post('/article', User.create);
+// router.put('/article', auth.required, user.updateUser);
+// router.get('/articles', auth.required, user.getUserAuthInfo);
+// router.get('/article/:id', auth.optional, user.getUserAuthInfo);
+
+// router.post('/comment', comment.create);
+// router.put('/comment', auth.required, comment.updateUser);
+// router.get('/comment', auth.required, comment.getUserAuthInfo);
+// router.get('/comment', auth.required, comment.getUserAuthInfo);
 
 export default router;
