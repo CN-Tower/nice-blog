ee041f07f47833b40ef77695f91d3e9c3dfe37d8
CN-Tower <cntower@yahoo.com>
2018-11-20 08:42:08 +0800

    Update comment.js

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,33 +2,38 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-class CommentSchema extends Schema {
-  constructor() {
-    super({
+const CommentSchema = new Schema({
+  owner:   { type: Schema.Types.ObjectId, ref: 'User' },
+  article: { type: Schema.Types.ObjectId, ref: 'Article', required: [true, "Can't be blank"], index: true },
+  total:   { type: Number, default: 0 },
+  body:    { type: String, required: [true, "Can't be blank"], trim: true },
+  replies: [
+    {
+      _id: Schema.Types.ObjectId,
       owner:   { type: Schema.Types.ObjectId, ref: 'User' },
-      article: { type: Schema.Types.ObjectId, ref: 'Article', required: [true, "Can't be blank"], index: true },
-      total:   { type: Number, default: 0 },
-      body:    { type: String, required: [true, "Can't be blank"], trim: true },
-      replies: [
-        {
-          _id: Schema.Types.ObjectId,
-          owner:   { type: Schema.Types.ObjectId, ref: 'User' },
-          retlyTo: { type: Schema.Types.ObjectId, ref: 'User' },
-          text: String,
-          createAt: Date,
-          updateAt: Date,
-          thumbUserIds: { type: Array, default: [] },
-        }
-      ]
-    }, { timestamps: true });
+      retlyTo: { type: Schema.Types.ObjectId, ref: 'User' },
+      text: String,
+      createAt: Date,
+      updateAt: Date,
+      thumbUserIds: { type: Array, default: [] },
+    }
+  ]
+}, {
+  timestamps: true,
+  collection: 'comments'
+});
 
-    this.methods = fn.extend({}, this, []);
-    this.statics = fn.extend({}, this, []);
+class CommentClass {
+  toJSONFor(user) {
+    return {
+      id: this._id,
+      body: this.body,
+      createdAt: this.createdAt,
+      author: this.author.toProfileJSONFor(user)
+    };
   }
-
-
 }
 
-const commentSchema = new CommentSchema();
+CommentSchema.loadClass(CommentClass);
 
-export default mongoose.model('Comment', commentSchema);
+export default mongoose.model('Comment', CommentSchema);
