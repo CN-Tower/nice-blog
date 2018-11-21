ba2be80dcbf91f0279bd6e5028df58b248b08202
CN-Tower <cntower@yahoo.com>
2018-11-21 13:00:46 +0800

    Update auth.js

--- server/settings/auth.js
+++ server/settings/auth.js
@@ -5,21 +5,24 @@ import config from 'config-lite';
 function getTokenFromHeader(req){
   const authorization = fn.get(req.headers, 'authorization');
   if (authorization && ['Token', 'Bearer'].includes(authorization.split(' ')[0])) {
-    authorization.split(' ')[1];
+    return authorization.split(' ')[1];
   }
   return null;
 }
 
 export default {
+
   required: jwt({
     secret: config.secret,
     userProperty: 'payload',
     getToken: getTokenFromHeader
   }),
+
   optional: jwt({
     secret: config.secret,
     userProperty: 'payload',
     credentialsRequired: false,
     getToken: getTokenFromHeader
   })
+
 };
