9e1e0f651582dac231f5b084bc9bfdb24c9a5036
CN-Tower <cntower@yahoo.com>
2018-11-21 12:56:01 +0800

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -7,40 +7,45 @@ import fn from 'funclib';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  name:      { type: String, required: [true, "Can't be blank"], unique: true, trim: true, index: true },
-  email:     { type: String, required: [true, "Can't be blank"], unique: true, trim: true, index: true },
-  phoneNb:   { type: String, unique: true, trim: true },
-  password:  { type: String, required: [true, "Can't be blank"] },
-  userType:  { type: Number, default: 1 },
-  avater:    { type: String, default: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png' },
-  address:   { type: String, default: '' },
-  company:   { type: String, default: '' },
-  webset:    { type: String, default: '' },
-  bio:       { type: String, default: '' },
-  webset:    { type: String, default: '' },
+  username:  { type: String,  required: [true, "Can't be blank"], unique: true, trim: true, index: true },
+  email:     { type: String,  required: [true, "Can't be blank"], unique: true, trim: true, index: true },
+  phoneNb:   { type: String,  unique: true, trim: true },
+  avater:    { type: String,  default: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png' },
+  address:   { type: String,  default: ''   },
+  company:   { type: String,  default: ''   },
+  webset:    { type: String,  default: ''   },
+  bio:       { type: String,  default: ''   },
+  webset:    { type: String,  default: ''   },
   articles:  [{ type: Schema.Types.ObjectId, ref: 'Article' }],
   favorites: [{ type: Schema.Types.ObjectId, ref: 'Article' }],
-  followers: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  following: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  followers: [{ type: Schema.Types.ObjectId, ref: 'User'    }],
+  following: [{ type: Schema.Types.ObjectId, ref: 'User'    }],
   comments:  [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-  replies:   { type: Array, default: [] },
-  messages:  { type: Array, default: [] },
-  activity:  { type: Number, default: 0 }
+  replies:   { type: Array,   default: []   },
+  messages:  { type: Array,   default: []   },
+  activity:  { type: Number,  default: 0    },
+  userType:  { type: Number,  default: 1    },
+  isforbid:  { type: Boolean, default: false},
+  salt: String,
+  hash: String,
 }, {
   timestamps: true,
   collection: 'users'
 });
 
 class UserClass {
-  validPassword(password) {
-    var hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+
+  async validPassword(password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 18, 'sha512').toString('hex');
     return this.hash === hash;
   }
-  setPassword(password) {
+
+  async setPassword(password) {
     this.salt = crypto.randomBytes(16).toString('hex');
-    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 18, 'sha512').toString('hex');
   }
-  generateJWT() {
+
+  async generateJWT() {
     var today = new Date();
     var exp = new Date(today);
     exp.setDate(today.getDate() + 60);
@@ -50,55 +55,58 @@ class UserClass {
       exp: parseInt(exp.getTime() / 1000),
     }, config.secret);
   }
-  favorite(id) {
+
+  async favorite(id) {
     if (!this.favorites.includes(id)) this.favorites.push(id);
     return this.save();
   }
+
   async unfavorite(id) {
     this.favorites.remove(id);
     return await this.save();
   }
-  isFavorite(id) {
+
+  async isFavorite(id) {
     return this.favorites.some(favoriteId => favoriteId.toString() === id.toString());
   }
-  follow(id) {
+
+  async follow(id) {
     if (!this.following.includes(id)) this.following.push(id);
     return this.save();
   }
+
   async unfollow(id) {
     this.following.remove(id);
     return await this.save();
   }
-  isFollowing(id) {
+
+  async isFollowing(id) {
     return this.following.some(followId => followId.toString() === id.toString());
   }
-  toAuthJSON() {
-    return {
-      id: this._id,
-      username: this.username,
-      email: this.email,
-      bio: this.bio,
-      avater: this.avater,
-      token: this.generateJWT(),
-    };
-  }
+
   toJSON() {
     return {
       id: this._id,
-      username: this.username,
       bio: this.bio,
+      username: this.username,
       avater: this.avater
     };
   }
-  toJSONFor(user) {
-    return {
-      id: this._id,
+
+  toProfileJSON() {
+    return fn.extend(this.toJSON(), {
       username: this.username,
-      bio: this.bio,
-      avater: this.avater,
-      following: user ? user.isFollowing(this._id) : false
-    };
+      email: this.email,
+      token: this.generateJWT()
+    });
   }
+
+  toJSONFor(user) {
+    fn.extend(this.toJSON(), {
+      following: user ? user.isFollowing(this._id) : false
+    });
+  }
+
 }
 
 UserSchema.loadClass(UserClass);
