c918c0e9ace1cdb8d69b076f2e6ba91d9e368bd4
CN-Tower <cntower@yahoo.com>
2018-11-21 12:53:36 +0800

    Update articles.js

---
+++ server/controller/articles.js
@@ -0,0 +1,80 @@
+import Article from '../models/article';
+import fn from 'funclib';
+
+class ArticleController {
+
+  async create (req, res, next) {
+    try {
+      return res.status(200).json({status: 1, message: 'ok'});
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  async action (req, res, next) {
+    try {
+      return res.status(200).json({status: 1, message: 'ok'});
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  async editArticle (req, res, next) {
+    try {
+      return res.status(200).json({status: 1, message: 'ok'});
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  async getInfo (req, res, next) {
+    try {
+      return res.status(200).json({status: 1, message: 'ok'});
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  async getComments (req, res, next) {
+    try {
+      return res.status(200).json({status: 1, message: 'ok'});
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  async getList (req, res, next) {
+    try {
+      return res.status(200).json({status: 1, message: 'ok'});
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  async getTags (req, res, next) {
+    try {
+      return res.status(200).json({status: 1, message: 'ok'});
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  async getHot (req, res, next) {
+    try {
+      return res.status(200).json({status: 1, message: 'ok'});
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+  async deleteArticle (req, res, next) {
+    try {
+      return res.status(200).json({status: 1, message: 'ok'});
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+};
+
+export default new ArticleController();
