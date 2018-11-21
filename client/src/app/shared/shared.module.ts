80015549ca7ca2e0ea2650f3c7f3084f64b9d2d6
CN-Tower <cntower@yahoo.com>
2018-11-21 17:45:30 +0800

    Update shared.module.ts

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
 import { registerLocaleData } from '@angular/common';
+import { SharedService } from './shared.service';
 import zh from '@angular/common/locales/zh';
 
 registerLocaleData(zh);
@@ -16,7 +17,10 @@ registerLocaleData(zh);
     NgZorroAntdModule
   ],
   declarations: [],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  providers: [
+    { provide: NZ_I18N, useValue: zh_CN },
+    SharedService
+  ],
   exports: [
     FormsModule,
     ReactiveFormsModule,
