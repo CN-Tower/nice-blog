81e5262c24f6912aa93b2d72df948394b77199b7
CN-Tower <cntower@yahoo.com>
2018-11-21 12:55:20 +0800

    Update article.js

--- server/models/article.js
+++ server/models/article.js
@@ -3,18 +3,17 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const ArticleSchema = new Schema({
-  title:     { type: String, required: [true, "can't be blank"] },
-  tabs:      { type: Array, default: [] },
+  title:       { type: String, required: [true, "can't be blank"] },
+  tabs:        { type: Array,  default: [] },
   description: { type: String, default: '' },
   body:      { type: String, required: [true, "can't be blank"] },
   owner:     { type: Schema.Types.ObjectId, ref: 'User', required: [true, "Can't be blank"], index: true },
   author:    { type: Schema.Types.ObjectId, ref: 'User' },
-  stars:     [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  favorites: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-  images:    { type: Array, default: [] },
   isRetweet: { type: Boolean, default: false },
-  collectedUserIds: { type: Array, default: [] },
-  commentId: { type: String, default: '' }
+  images:    [{ type: String }],
+  comments:  [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  likes:     [{ type: Schema.Types.ObjectId, ref: 'User'    }],
+  favorites: [{ type: Schema.Types.ObjectId, ref: 'User'    }]
 }, {
   timestamps: true,
   collection: 'articles'
