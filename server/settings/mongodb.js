eed6725d21a8d5f0287ce3a5ad730cc53cd7418f
CN-Tower <cntower@yahoo.com>
2018-11-23 16:35:16 +0800

    Update mongodb.js

--- server/settings/mongodb.js
+++ server/settings/mongodb.js
@@ -2,21 +2,21 @@ import mongoose from 'mongoose';
 import config from 'config-lite';
 import fn from 'funclib';
 
-mongoose.connect(config.dbUrl, { useMongoClient: true });
 mongoose.Promise = global.Promise;
-
 const db = mongoose.connection;
 
-db.once('open', () => {
-  fn.log('Connect MongoDB success!', { title: 'Msg from MongoDB', color: 'green' });
+db.on('connected', function() {
+  fn.log('Connect to MongoDB success!', { title: 'MongoDB', color: 'green' });
 });
 
-db.on('error', function (error) {
-  fn.log('Error in MongoDB connection: ' + error, { title: 'Msg from MongoDB', color: 'red' });
+db.on('error', err => {
+  fn.log(`Connect to MongoDB failed: ${err}`, { title: 'MongoDB', color: 'red' });
   mongoose.disconnect();
 });
 
-db.on('close', function () {
-  fn.log('MongoDB was Disconnected, now is reconnecting...', { title: 'Msg from MongoDB', color: 'red' });
-  mongoose.connect(config.url, { server: { auto_reconnect: true } });
+db.on('disconnected', () => {
+  fn.log('Reconnecting to MongoDB...', { title: 'MongoDB', color: 'green' });
+  fn.timeout(() => mongoose.connect(config.dbUrl, { useNewUrlParser: true }), 5000);
 });
+
+mongoose.connect(config.dbUrl, { useNewUrlParser: true }); 
